5752a49d565fe85728ee5207809ac6a945b336d5
Aleksey Beketov <alex311271@gmail.com>
2022-11-27 14:09:18 +0300

    return back

--- src/test/java/ru/netology/service/CashbackHackerServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackerServiceTest.java
@@ -10,7 +10,7 @@ public class CashbackHackerServiceTest {
     public void shouldCashBackHackerServiceReturn0() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(1000);
-        int expected = 1000;
+        int expected = 0;
         Assert.assertEquals(actual, expected);
     }
 
